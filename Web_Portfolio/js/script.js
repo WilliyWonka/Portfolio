ca2a3ba0bd4dce8821bec3b24b00a5802bf01887
William Wonki Hong <72485918+WilliyWonka@users.noreply.github.com>
2022-08-12 11:45:52 +0900

    Build (HTML/CSS/JS) Basic templete.
    자바스크립트 : 
    Color theme change
    Black&White page change.

---
+++ Web_Portfolio/js/script.js
@@ -0,0 +1,8 @@
+/* ========================typing animation ======================== */
+/* PS : 3:02:27 */
+var typed = new Typed(".typing",{
+    strings:["","Junior Developer","Web Developer","Pro Marketer"],
+    typeSpeed: 90,
+    BackSpeed: 40,
+    loop:true
+})
